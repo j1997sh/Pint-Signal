84f10b1b8e9295fac9c3895b3af9bc670d53fc5c
j1997sh <jamesalhiggs@icloud.com>
2026-04-20 19:39:24 +0100

    Update signals.js

--- signals.js
+++ signals.js
@@ -2,60 +2,70 @@ export const signals = [
   {
     id: "parliament",
     name: "Parliament Signal",
+    districtId: "whitehall-st-jamess",
     lat: 51.4995,
     lng: -0.1248
   },
   {
     id: "trafalgar",
     name: "Trafalgar Signal",
+    districtId: "whitehall-st-jamess",
     lat: 51.5080,
     lng: -0.1281
   },
   {
-    id: "westminster-bridge",
+    id: "bridge",
     name: "Bridge Signal",
+    districtId: "covent-garden-strand",
     lat: 51.5009,
     lng: -0.1217
   },
   {
-    id: "green-park",
+    id: "green",
     name: "Green Park Signal",
+    districtId: "mayfair-belgravia",
     lat: 51.5065,
     lng: -0.1426
   },
   {
     id: "buckingham",
     name: "Palace Signal",
+    districtId: "mayfair-belgravia",
     lat: 51.5014,
     lng: -0.1419
   },
   {
     id: "leicester",
     name: "Leicester Signal",
+    districtId: "covent-garden-strand",
     lat: 51.5113,
     lng: -0.1281
   },
   {
     id: "soho",
     name: "Soho Signal",
+    districtId: "soho-piccadilly",
     lat: 51.5136,
     lng: -0.1365
   },
   {
     id: "waterloo",
-    name: "Waterloo Signal",
+    name: "Waterloo Edge Signal",
+    districtId: "covent-garden-strand",
     lat: 51.5033,
     lng: -0.1147
   },
   {
-    id: "vauxhall",
-    name: "Vauxhall Signal",
+    id: "vauxhall-edge",
+    name: "Pimlico Edge Signal",
+    districtId: "westminster-victoria",
     lat: 51.4866,
     lng: -0.1225
   },
   {
     id: "victoria",
     name: "Victoria Signal",
+    districtId: "westminster-victoria",
     lat: 51.4952,
     lng: -0.1442
   }
