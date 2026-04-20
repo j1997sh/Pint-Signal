3ef2b733246abd306a94b6c823442e4dbddc0a49
j1997sh <jamesalhiggs@icloud.com>
2026-04-20 19:20:43 +0100

    Create signals.js

---
+++ signals.js
@@ -0,0 +1,62 @@
+export const signals = [
+  {
+    id: "parliament",
+    name: "Parliament Signal",
+    lat: 51.4995,
+    lng: -0.1248
+  },
+  {
+    id: "trafalgar",
+    name: "Trafalgar Signal",
+    lat: 51.5080,
+    lng: -0.1281
+  },
+  {
+    id: "westminster-bridge",
+    name: "Bridge Signal",
+    lat: 51.5009,
+    lng: -0.1217
+  },
+  {
+    id: "green-park",
+    name: "Green Park Signal",
+    lat: 51.5065,
+    lng: -0.1426
+  },
+  {
+    id: "buckingham",
+    name: "Palace Signal",
+    lat: 51.5014,
+    lng: -0.1419
+  },
+  {
+    id: "leicester",
+    name: "Leicester Signal",
+    lat: 51.5113,
+    lng: -0.1281
+  },
+  {
+    id: "soho",
+    name: "Soho Signal",
+    lat: 51.5136,
+    lng: -0.1365
+  },
+  {
+    id: "waterloo",
+    name: "Waterloo Signal",
+    lat: 51.5033,
+    lng: -0.1147
+  },
+  {
+    id: "vauxhall",
+    name: "Vauxhall Signal",
+    lat: 51.4866,
+    lng: -0.1225
+  },
+  {
+    id: "victoria",
+    name: "Victoria Signal",
+    lat: 51.4952,
+    lng: -0.1442
+  }
+];
